3ad4284e3144112c546a55159d70c8b05c4dbf82
Igor Dronnikov <igor.dronnikov@axamit.com>
2019-05-28 11:41:17 +0300

    Added fields for user

--- src/main/java/com/axamit/billingapp/model/User.java
+++ src/main/java/com/axamit/billingapp/model/User.java
@@ -11,29 +11,64 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
+    private Long phone;
+    private String email;
+    private String password;
     private double bill;
     public User() {
     }
-    public User(Long id, String name, double bill) {
-        this.id = id;
+    public User(String name, Long phone, String email, String password, double bill) {
         this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
         this.bill = bill;
     }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = String.valueOf(password.hashCode());
+    }
+
     public double getBill() {
         return bill;
     }
+
     public void setBill(double bill) {
         this.bill = bill;
     }
